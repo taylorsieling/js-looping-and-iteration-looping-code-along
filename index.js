b432dec9bcf3494678baaa6c56304ce5cb417f03
taylorsieling <taylorasieling@gmail.com>
2021-02-23 16:50:18 -0600

    commit tree 77d82b744304adc4b6a39c93e56ee6757c946758

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(friends, gift) {
+    let cards = [];
+    for (let i = 0; i < friends.length; i++) {
+        cards.push(`Thank you, ${friends[i]}, for the wonderful ${gift} gift!`);
+    }
+    return cards;
+}
+
+// writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
